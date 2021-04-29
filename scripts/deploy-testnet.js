1d4d18811068328260807719f043d99f3e61540b
trinhtan <lop12a1trinhvantan@gmail.com>
2021-04-29 15:49:07 +0700

    fix script deploy

--- scripts/deploy-testnet.js
+++ scripts/deploy-testnet.js
@@ -2,18 +2,18 @@ const { ethers, network } = require('hardhat');
 require('@nomiclabs/hardhat-ethers');
 
 async function main() {
-  if (network.name !== 'ropsten' && network.name != 'rinkeby') {
+  if (network.name !== 'ropsten' && network.name !== 'rinkeby') {
     throw new Error('Invalid network');
   }
+
   let [deployer] = await ethers.getSigners();
 
-  let amount = '8000000000000000000000000'; // 8m moma
+  let amount = '8000000000000000000000000'; // 8m MOMA
   let farmFactory, farmGenerator, farm;
 
-  let rewardPerBlock = '3000000000000000000';
+  let rewardPerBlock = '3000000000000000000'; // 3 MOMA/block
 
   let startBlock = (await ethers.provider.getBlockNumber()) + 20;
-  console.log('Startblock: ', startBlock);
   let firstCycleRate = 2;
   let initRate = 1;
   let reducingRate = 95;
@@ -22,16 +22,22 @@ async function main() {
   let percentForVesting = 100;
   let vestingDuration = 1170000; // 6 months
 
-  let momaToken, weth, lpToken, uniswapV2Factory, uniswapV2Router;
+  let momaTokenAddress = '0x78a369C94C7f5dFbD89a26DB1b9a398aD14B5b0A';
   let uniswapV2RouterAddress = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D';
+
+  let momaToken, weth, lpToken, uniswapV2Factory, uniswapV2Router;
+  let tx;
 
   console.log('Deploy contract with the account: ', deployer.address);
 
-  console.log('\nDeploy Test MOMA');
-  let TestERC20 = await ethers.getContractFactory('TestERC20');
-  momaToken = await TestERC20.connect(deployer).deploy('MOchi MArket Token', 'MOMA');
-  await momaToken.deployed();
-  await momaToken.connect(deployer).mint(deployer.address, amount);
+  if (momaTokenAddress === '' || momaTokenAddress === undefined) {
+    let TestERC20 = await ethers.getContractFactory('TestERC20');
+    momaToken = await TestERC20.connect(deployer).deploy('MOchi MArket Token', 'MOMA');
+
+    await momaToken.connect(deployer).mint(deployer.address, amount);
+  } else {
+    momaToken = await ethers.getContractAt('IERC20', momaTokenAddress);
+  }
 
   uniswapV2Router = await ethers.getContractAt('IUniswapV2Router', uniswapV2RouterAddress);
 
@@ -42,10 +48,10 @@ async function main() {
     await uniswapV2Router.factory()
   );
 
-  let tx = await uniswapV2Factory.connect(deployer).createPair(momaToken.address, weth.address);
-  await tx.wait();
-
-  lpToken = await uniswapV2Factory.getPair(momaToken.address, weth.address);
+  lpToken = await ethers.getContractAt(
+    'IUniswapV2Pair',
+    await uniswapV2Factory.getPair(momaToken.address, weth.address)
+  );
 
   console.log('\nDeploy Farm Factory...');
   let FarmFactory = await ethers.getContractFactory('FarmFactory');
@@ -60,29 +66,31 @@ async function main() {
   );
   await farmGenerator.deployed();
 
-  await farmFactory.connect(deployer).adminAllowFarmGenerator(farmGenerator.address, true);
+  tx = await farmFactory.connect(deployer).adminAllowFarmGenerator(farmGenerator.address, true);
+  await tx.wait();
 
   console.log('\nCreate Farm...');
-
   tx = await momaToken.connect(deployer).approve(farmGenerator.address, amount);
   await tx.wait();
+
   tx = await farmGenerator
     .connect(deployer)
     .createFarm(
       momaToken.address,
       amount,
-      lpToken,
+      lpToken.address,
       rewardPerBlock,
       startBlock,
+      deployer.address,
       [firstCycleRate, initRate, reducingRate, reducingCycle],
       [percentForVesting, vestingDuration]
     );
-
   await tx.wait();
 
   farm = await ethers.getContractAt('Farm', await farmFactory.farmAtIndex(0));
   console.log('\n\nAll setup successfully...');
-  console.log('LP Token: ', lpToken);
+  console.log('Startblock: ', startBlock);
+  console.log('LP Token: ', lpToken.address);
   console.log('MOMA: ', momaToken.address);
   console.log('WETH: ', weth.address);
   console.log('Farm Factory: ', farmFactory.address);
